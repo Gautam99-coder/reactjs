2f7dd342464c4b3cb40d7fa25ae9bc73c2833dbf
Gautam99-coder <gautamtharu1000@gmail.com>
2025-05-26 22:27:49 +0545

    this is array with obj props 3

--- myfirst-react/src/App.jsx
+++ myfirst-react/src/App.jsx
@@ -1,37 +1,20 @@
 const Hello = (props) => {
-
-  console.log(props)
-  return (
-    <div>
-      <p>
-
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  )
-}
-const Footer =(props)=>{
-  console.log(props)
+  const Student=[
+    {name:"Aja",id:1},
+    {name:"spider", id:2},
+  ]
   return(
     <div>
-      <p>this is footer ,this is my social handle<a href="www.facebook.com">facebook</a></p>
+      <p>{Student[0].name} {Student[1].name}</p>
     </div>
   )
 }
 
 const App = () => {
-
-  const name = 'spider'
-  const age = 99
-
   return (
     <div>
       <h1>Greetings</h1>
-
-      <Hello name='Maya' age={26 + 10} />
-      <Hello name={name} age={age} />
-      <Hello name='batman' age={35} />
-      <Footer/>
+      <Hello/>
     </div>
   )
 }
