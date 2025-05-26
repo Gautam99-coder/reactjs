dea16ef76e6ad46a710c8f5d69112abf3691631b
Gautam99-coder <gautamtharu1000@gmail.com>
2025-05-26 22:16:35 +0545

    this is the props 2

--- myfirst-react/src/App.jsx
+++ myfirst-react/src/App.jsx
@@ -1,18 +1,37 @@
-const House = (props) =>{
-  console.log(props);
+const Hello = (props) => {
+
+  console.log(props)
+  return (
+    <div>
+      <p>
+
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+const Footer =(props)=>{
+  console.log(props)
   return(
     <div>
-      <p>house {props.name}</p>
+      <p>this is footer ,this is my social handle<a href="www.facebook.com">facebook</a></p>
     </div>
   )
 }
 
 const App = () => {
+
+  const name = 'spider'
+  const age = 99
+
   return (
     <div>
       <h1>Greetings</h1>
-      <House name='spider'/>
-      <House/>
+
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
+      <Hello name='batman' age={35} />
+      <Footer/>
     </div>
   )
 }
