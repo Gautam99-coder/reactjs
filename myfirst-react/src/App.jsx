5958d07e59eb5e3df263772253a4dfc0372c792f
Gautam99-coder <gautamtharu1000@gmail.com>
2025-05-27 10:11:23 +0545

    components helper function

--- myfirst-react/src/App.jsx
+++ myfirst-react/src/App.jsx
@@ -1,64 +1,30 @@
-import React from 'react';
-
-const Header = (props) => {
-  console.log('Header props:', props); // e.g., { course: 'Half Stack application development' }
-  return <h1>{props.course}</h1>;
-};
-
-const Part = (props) => {
-  console.log('Part props:', props); // e.g., { name: 'Fundamentals of React', exercises: 10 }
-  return (
-    <p>
-      {props.name} {props.exercises}
-    </p>
-  );
-};
-
-const Content = (props) => {
-  console.log('Content props:', props); // e.g., { part1: {...}, part2: {...}, part3: {...} }
+const Hello = (props) => {
+  const bornYear=()=>{
+      const yearNow=new Date().getFullYear();
+      return yearNow - props.age;
+  }
   return (
     <div>
-      <Part name={props.part1.name} exercises={props.part1.exercises} />
-      <Part name={props.part2.name} exercises={props.part2.exercises} />
-      <Part name={props.part3.name} exercises={props.part3.exercises} />
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+      <p>
+        So you are probably born in {bornYear()}
+      </p>
     </div>
-  );
-};
-
-const Total = (props) => {
-  console.log('Total props:', props); // e.g., { exercises1: 10, exercises2: 7, exercises3: 14 }
-  const totalExercises = props.exercises1 + props.exercises2 + props.exercises3;
-  return (
-    <p>Number of exercises {totalExercises}</p>
-  );
-};
+  )
+}
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  };
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  };
+  const name = 'Peter'
+  const age = 10
 
   return (
     <div>
-      <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total 
-        exercises1={part1.exercises} 
-        exercises2={part2.exercises} 
-        exercises3={part3.exercises} 
-      />
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
-  );
-};
-
+  )
+}
 export default App;
