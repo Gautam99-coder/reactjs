9ce3e82f089bf8036f35771a1dacb8d444527409
Gautam99-coder <gautamtharu1000@gmail.com>
2025-05-27 08:43:17 +0545

    props excesices

--- myfirst-react/src/App.jsx
+++ myfirst-react/src/App.jsx
@@ -1,21 +1,30 @@
-const Hello = (props) => {
-  const Student=[
-    {name:"Aja",id:1},
-    {name:"spider", id:2},
-  ]
-  return(
+const App = () => {
+  const a=[1,2,3,4,5];
+  const [x,y,z,...rest]=a;
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
     <div>
-      <p>{Student[0].name} {Student[1].name}</p>
+      <p>{x} {y} {z}</p>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
 }
 
-const App = () => {
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello/>
-    </div>
-  )
-}
 export default App
