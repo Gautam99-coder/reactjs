0f3638d2aba35a158b2556d26480195f25f5fdfd
Gautam99-coder <gautamtharu1000@gmail.com>
2025-05-28 22:18:36 +0545

    event handling function

--- myfirst-react/src/App.jsx
+++ myfirst-react/src/App.jsx
@@ -3,22 +3,16 @@ import { useState } from 'react'
 const App = () => {
 
   const [ counter, setCounter ] = useState(0)
-  const handleClick=()=>{
-    console.log('clicked')
-  }
-
-
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
+  // setTimeout(
+  //   () => setCounter(counter + 1),
+  //   1000
+  // )
   console.log('rendring...',counter)
 
   return (
     <div>
     <div>{counter}</div>
-    <button onClick={handleClick}
->
+    <button onClick={()=>setCounter(counter+1)} >
       Puls
     </button>
     </div>
