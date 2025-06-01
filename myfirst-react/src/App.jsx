d8d2d80d1cbfb08fbffd1af2f6e1eb8005761c10
Gautam99-coder <gautamtharu1000@gmail.com>
2025-06-01 13:12:29 +0545

    handling arrays

--- myfirst-react/src/App.jsx
+++ myfirst-react/src/App.jsx
@@ -13,32 +13,30 @@ const Button = (props) => {
   )
 }
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
+  const [left,setLeft]=useState(0)
+  const [right,setRight]=useState(0)
+  const [allClicks,setAll]=useState([]);
+  const [total,setTotal]=useState(0)
 
-  const handleLeftClick = () => {
-    const newClicks = { 
-      left: clicks.left + 1, 
-      right: clicks.right 
-    }
-    setClicks(newClicks)
+  const HandlLeftClicks=()=>{
+    setAll(allClicks.concat('L'))
+    setLeft(left+1)
+    setTotal(left+right)
   }
-
-  const handleRightClick = () => {
-    const newClicks = { 
-      left: clicks.left, 
-      right: clicks.right + 1 
-    }
-    setClicks(newClicks)
+   const HandlRightClicks=()=>{
+    setAll(allClicks.concat('R'))
+    setRight(right+1)
+    setTotal(left+right)
   }
 
   return (
     <div>
-      {clicks.left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {clicks.right}
+      {left}
+      <button onClick={HandlLeftClicks}>left</button>
+      {right}
+      <button onClick={HandlRightClicks}>right</button>
+      <p>{allClicks.join('')}</p>
+      <p>total: {total}</p>
     </div>
   )
 }
